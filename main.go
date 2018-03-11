3726dfd667fa7b81f2799d927fc2de387c73f247
Alvaro Aleman <alv2412@googlemail.com>
2018-03-11 17:40:56 +0100

    Allow using tls for calling amqp

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/streadway/amqp"
 )
@@ -15,19 +18,57 @@ func main() {
 	amqpUser := os.Getenv("AMQP_USER")
 	amqpPass := os.Getenv("AMQP_PASS")
 	amqpHost := os.Getenv("AMQP_HOST")
-	amqpConnectionString := fmt.Sprintf("amqp://%s:%s@%s/", amqpUser, amqpPass, amqpHost)
 
-	amqpConnection, err := amqp.Dial(amqpConnectionString)
-	defer amqpConnection.Close()
+	clientCert := os.Getenv("AMQP_CLIENCERTFILE")
+	clientKey := os.Getenv("AMQP_CLIENTKEYFILE")
+	caCertFilePath := os.Getenv("AMQP_CACERT")
+
+	amqpScheme := "amqp"
+
+	var tlsConfig *tls.Config
+	if clientCert != "" && clientKey != "" && caCertFilePath != "" {
+		amqpScheme = "amqps"
+		log.Printf("Loading client cert and key..")
+		cert, err := tls.LoadX509KeyPair(clientCert, clientKey)
+		if err != nil {
+			log.Fatalf("Error loading client cert and key: '%s'", err)
+		}
+
+		log.Printf("Loading cacert...")
+		caCert, err := ioutil.ReadFile(caCertFilePath)
+		if err != nil {
+			log.Fatalf("Error reading cacert: '%s'", err)
+		}
+
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(caCert)
+
+		tlsConfig = &tls.Config{
+			Certificates: []tls.Certificate{cert},
+			RootCAs:      caCertPool,
+			ServerName:   fmt.Sprintf("AMQP/%s", strings.Split(amqpHost, ":")[0]),
+		}
+	}
+
+	amqpConnectionString := fmt.Sprintf("%s://%s:%s@%s/", amqpScheme, amqpUser, amqpPass, amqpHost)
+
+	var amqpConnection *amqp.Connection
+	var err error
+	if amqpScheme == "amqp" {
+		amqpConnection, err = amqp.Dial(amqpConnectionString)
+	} else if amqpScheme == "amqps" {
+		amqpConnection, err = amqp.DialTLS(amqpConnectionString, tlsConfig)
+	}
 	if err != nil {
 		log.Fatalf("Error creating amqp connection: '%s'", err)
 	}
+	defer amqpConnection.Close()
 
 	amqpChannel, err := amqpConnection.Channel()
-	defer amqpChannel.Close()
 	if err != nil {
 		log.Fatalf("Error creating amqp channel: '%s'", err)
 	}
+	defer amqpChannel.Close()
 
 	amqpQueue, err := amqpChannel.QueueDeclare(queueName, false, false, false, false, nil)
 	if err != nil {
